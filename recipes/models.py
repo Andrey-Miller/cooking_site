ffa954a7e3289f967db26d7e12c79e88cc5987a2
sinedd <sineddam@mail.ru>
2024-05-08 23:04:43 +0500

    models added, db modified

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name='Название')
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Название')
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(verbose_name='Описание')
+    cooking = models.TextField(verbose_name='Приготовление')
+    cooking_time = models.PositiveSmallIntegerField(default=0, verbose_name='Время приготовления')
+    image = models.ImageField(upload_to='images/', default='default.png', blank=True, verbose_name='Изображение')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    products = models.TextField(verbose_name='Продукты', default='')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, verbose_name='Категория')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'recipe'
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.TextField(verbose_name='Адрес')
+    registration_date = models.DateField(auto_now_add=True, verbose_name='Дата регистрации')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+
